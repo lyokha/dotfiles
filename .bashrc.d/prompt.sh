a8a91ef61187a3720b84429b7697683f4296f699
Alexey Radkov <alexey.radkov@gmail.com>
2023-11-15 01:31:59 +0400

    show git toplevel directory in bash prompt

--- .bashrc.d/prompt.sh
+++ .bashrc.d/prompt.sh
@@ -1,23 +1,46 @@
+function prompt_command
+{
+    if [ "$PROMPT_OLDPWD" != "$PWD" ]; then
+        PROMPT_GIT_TOPLEVEL=$(git rev-parse --show-toplevel 2>/dev/null)
+        PROMPT_OLDPWD=$PWD
+    fi
+}
+
+function prompt_gitdir
+{
+    [ -n "$PROMPT_GIT_TOPLEVEL" ] && echo " 󰊢 ${PROMPT_GIT_TOPLEVEL##*/}"
+}
+
+function prompt_wdir
+{
+    if [ -z "$PROMPT_GIT_TOPLEVEL" ] || [ "$PROMPT_GIT_TOPLEVEL" != "$PWD" ]
+    then
+        [ "$HOME" = "$PWD" ] && echo " ~" || echo " ${PWD##*/}"
+    fi
+}
+
 function prompt_line
 {
     local DATE_COLOR=$1
     local USER_COLOR=$2
     local AT_COLOR=$3
     local HOST_COLOR=$4
-    local DIR_COLOR=$5
-    local PS2_COLOR=$6
+    local GIT_COLOR=$5
+    local DIR_COLOR=$6
+    local PS2_COLOR=$7
 
     local N_COLORS
     N_COLORS=$(tput colors 2>/dev/null)
 
-    # BEWARE: values of ${HOSTNAME} and \h differ (at least in my Fedora 37)
+    # BEWARE: values of ${HOSTNAME} and \h may differ (they do in my Fedora 37)
     if (( N_COLORS >= 256 )); then
         local PROMPT_LINE="\
 \[\e]0;\u@${HOSTNAME}:\w\a\]\
 \[\e[38;5;${DATE_COLOR}m\]\$(date +%d/%m/%y\ %H:%M) \
 \[\e[38;5;${USER_COLOR}m\]\u\[\e[38;5;${AT_COLOR}m\]@\
 \[\e[38;5;${HOST_COLOR}m\]${HOSTNAME}\
-\[\e[38;5;${DIR_COLOR}m\] \W \[\e[0m\] "
+\[\e[38;5;${GIT_COLOR}m\]\$(prompt_gitdir)\
+\[\e[38;5;${DIR_COLOR}m\]\$(prompt_wdir) \[\e[0m\] "
         local PROMPT_LINE2="\[\e[38;5;${PS2_COLOR}m\]\[\e[0m\] "
     elif (( N_COLORS >= 8 )); then
         local PROMPT_LINE="\
@@ -31,10 +54,11 @@ function prompt_line
 
     PS1=$PROMPT_LINE
     PS2=$PROMPT_LINE2
+
     PROMPT_DIRTRIM=2
+    PROMPT_COMMAND=prompt_command
 }
 
-prompt_line 167 173 167 140 173 196
+prompt_line 167 173 167 140 180 173 196
 unset prompt_line
-unset PROMPT_COMMAND
 
